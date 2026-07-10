6a2ccc22ddc943cd4daad8e7e58714eb2a7244ed
Md. Shazzad Hossain Nabil <73520759+naabilll@users.noreply.github.com>
2026-07-10 16:55:51 +0600

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -77,7 +77,7 @@
             openEmbedModal(busLat, busLng, BUFT_LAT, BUFT_LNG, "🚍 Bus to University");
         }
 
-        const SERVER = "/api";
+        const SERVER = "https://bus-server-lr2x.onrender.com"; 
 
         // UNIQUE USER ID FOR TROLL PREVENTION
         let localUserId = localStorage.getItem('buft_user_id');
